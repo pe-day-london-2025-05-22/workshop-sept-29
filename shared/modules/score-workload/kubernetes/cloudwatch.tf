86897b51ef6e8b5632cf7c84e02cf084c5c1ceee
Ben Meier <ben.meier@humanitec.com>
2025-09-16 12:05:16 +0100

    chore: cloudwatch attribute

---
+++ shared/modules/score-workload/kubernetes/cloudwatch.tf
@@ -0,0 +1,20 @@
+data "kubernetes_nodes" "nodes" {
+}
+
+locals {
+    node_labels = data.kubernetes_nodes.nodes.nodes[0].metadata[0].labels
+    cluster_region = lookup(local.node_labels, "topology.kubernetes.io/region", "unknown")
+    cluster_name = lookup(local.node_labels, "alpha.eksctl.io/cluster-name", "unknown")
+
+    cloud_watch_query = replace(urlencode(<<EOT
+SELECT `@timestamp`, `@message`
+FROM `/aws/containerinsights/${local.cluster_name}/application`
+WHERE `@entity.Attributes.K8s.Namespace` = '${var.namespace}'
+  AND `@entity.Attributes.K8s.Workload` = '${var.metadata.name}'
+ORDER BY `@timestamp` DESC
+LIMIT 1000;
+EOT
+), "%", "*")
+
+    cloud_watch_url = local.cluster_name == "unknown" ? "" : "https://${local.cluster_region}.console.aws.amazon.com/cloudwatch/home?region=${cluster_region}#logs-insights$3FqueryDetail$3D~(end~0~start~-3600~timeType~'RELATIVE~tz~'UTC~unit~'seconds~editorString~'${local.cloud_watch_query}'"
+}
